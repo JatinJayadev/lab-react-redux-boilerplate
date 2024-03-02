8850844f651ee8b4d9a8abd978fca21aea21dba3
Jatin <jatinjayadev@gmail.com>
2024-03-02 18:13:00 +0530

    DOne

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './App.css'
+import LikeCounter from './Redux/LikeCounter'
+
+const App = () => {
+  return (
+    <>
+      <LikeCounter />
+    </>
+  )
+}
+
+
+
+export default App
